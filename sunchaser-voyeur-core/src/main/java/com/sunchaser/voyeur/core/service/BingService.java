b31000d4351e9b65e0913d796f9eb2d09275a641
SunChaser <admin@lilu.org.cn>
2020-11-22 21:57:55 +0800

    add webmagic

--- sunchaser-voyeur-core/src/main/java/com/sunchaser/voyeur/core/service/BingService.java
+++ sunchaser-voyeur-core/src/main/java/com/sunchaser/voyeur/core/service/BingService.java
@@ -12,11 +12,41 @@ import org.springframework.stereotype.Service;
  * format，非必要。返回结果的格式，不存在或者等于xml时，输出为xml格式，等于js时，输出json格式
  * idx，非必要。不存在或者等于0时，输出当天的图片，-1为已经预备用于明天显示的信息，1则为昨天的图片，以此类推，idx最多获取到前16天的图片信息
  *
+ * 返回示例
+ * {
+ *     "images":[
+ *         {
+ *             "startdate":"20201121",
+ *             "fullstartdate":"202011211600",
+ *             "enddate":"20201122",
+ *             "url":"/th?id=OHR.Dromling_ZH-CN0730577626_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+ *             "urlbase":"/th?id=OHR.Dromling_ZH-CN0730577626",
+ *             "copyright":"Drömling湿地中的灰鹤，德国 (© Mike Friedrichs/Getty Images)",
+ *             "copyrightlink":"https://www.bing.com/search?q=Dr%C3%B6mling%E6%B9%BF%E5%9C%B0&form=hpcapt&mkt=zh-cn",
+ *             "title":"",
+ *             "quiz":"/search?q=Bing+homepage+quiz&filters=WQOskey:%22HPQuiz_20201121_Dromling%22&FORM=HPQUIZ",
+ *             "wp":true,
+ *             "hsh":"442e2b414a95e8120a8c5e94c9399c54",
+ *             "drk":1,
+ *             "top":1,
+ *             "bot":1,
+ *             "hs":[
+ *
+ *             ]
+ *         }
+ *     ],
+ *     "tooltips":{
+ *         "loading":"正在加载...",
+ *         "previous":"上一个图像",
+ *         "next":"下一个图像",
+ *         "walle":"此图片不能下载用作壁纸。",
+ *         "walls":"下载今日美图。仅限用作桌面壁纸。"
+ *     }
+ * }
  * @date 2020/11/17
  * @since JDK 1.8
  */
 @Service
 public class BingService {
 
-    public
 }
---
+++ sunchaser-voyeur-integration/src/main/java/com/sunchaser/voyeur/integration/BingIntegration.java
@@ -0,0 +1,13 @@
+package com.sunchaser.voyeur.integration;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author admin@lilu.org.cn
+ * @description
+ * @date 2020/11/22
+ * @since JDK 1.8
+ */
+@Component
+public class BingIntegration {
+}
